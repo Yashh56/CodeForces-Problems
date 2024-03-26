c767a37587a5c3391d4f0613ae4e8fac84cecac3
Yash <yashsaini18166@gmail.com>
2024-03-26 21:41:22 +0530

    TLE 800

--- TLE/Beginners800/AmbitiousKid.cpp
+++ TLE/Beginners800/AmbitiousKid.cpp
@@ -1,54 +1,26 @@
-// #include <iostream>
-// #include <algorithm>
-// using namespace std;
-
-// void solve() {
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     for (int i = 0; i < n; i++) {
-//         cin >> arr[i];
-//     }
-//     for (int i = 0; i < n; i++) {
-//         abs(arr[i]);
-//     }
-//     sort(arr, arr + n);
-//     cout << arr[0] << endl;
-// }
-
-// int main() {
-//     int t;
-//     cin >> t;
-//     while (t-- > 0) {
-//         solve();
-//     }
-//     return 0;
-// }
-
 #include <iostream>
-#include <cmath> // For std::abs
+#include <cmath>
 using namespace std;
 
 void solve()
 {
     int n;
     cin >> n;
-    int nums[n];
-    for (int i = 0; i < n; ++i)
+    long long  mini = INT_MAX;
+    for (int i = 0; i < n; i++)
     {
-        nums[i] = std::abs(nums[i]);
+        long long t;
+        cin>>t;
+        if(t==0){
+            mini=0;
+            break;
+        }
+        mini=min(mini,abs(t));
     }
-
-    cout << nums[0] << endl;
-}
+    cout<<mini<<endl;
+    }
 
 int main()
 {
     solve();
-    // int t;
-    // cin>>t;
-    // while(t-->0){
-    //     solve();
-    // }
-    // return 0;
 }
