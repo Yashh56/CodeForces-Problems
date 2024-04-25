0663f61a8d326139b51c826c95f4ec042760a403
Yash <yashsaini18166@gmail.com>
2024-04-25 14:27:53 +0530

    DP CP and TLE 800

--- CSES/DP/Coin CombinationII.cpp
+++ CSES/DP/Coin CombinationII.cpp
@@ -2,9 +2,12 @@
 
 using namespace std;
 
+#define MOD 1000000007
+
 int main()
 {
     int n, x;
+    // int MOD = 1e9;
     cin >> n >> x;
     vector<int> a(n);
 
@@ -13,7 +16,29 @@ int main()
         cin >> a[i];
     }
 
-    vector<int>dp(n+1,vector<int>(x+1,0));
-    dp[0][0] = 1;
-    
+    vector<vector<int>>dp(n+1,vector<int>(x+1));
+
+    for (int i = 0; i < n; i++)
+    {
+        dp[i][0] = 1;
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int sum = 1; sum <= x; sum++)
+        {
+            int skip = dp[i + 1][sum];
+
+            int pick=0;
+
+            if(a[i]<=sum){
+                pick=dp[i][sum-a[i]];
+            }
+
+            dp[i][sum] = (skip+pick) % MOD;
+        }
+    }
+
+    cout<<dp[0][x]<<endl;
+    return 0;
 }
