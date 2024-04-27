ed46a645ebbc51efb2ccd1f1ead94f2da3d4d6fe
Yash <yashsaini18166@gmail.com>
2024-04-27 17:46:06 +0530

    DP CSES CF

--- 800/NextRound.java
+++ 800/NextRound.java
@@ -16,7 +16,7 @@ public class NextRound {
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= arr[k - 1] && arr[i] != 0) {
-                System.out.println(arr[k-1]);
+                // System.out.println(arr[k-1]);
                 count++;
             }
         }
