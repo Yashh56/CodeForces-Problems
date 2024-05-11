cdc0162140fe055dc472d510271f83cc537da6b0
Yash <yashsaini18166@gmail.com>
2024-05-11 23:51:35 +0530

    TLE 900

--- TLE/900/Not Dividing.cpp
+++ TLE/900/Not Dividing.cpp
@@ -37,28 +37,36 @@ bool even(ll num) { return ((num & 1) == 0); }
 
 void solve()
 {
-    
-    ll n,a[200005],q,sum=0,pref[200005],t;
-    cin>>n>>q;
+
+    ll n;
+    cin >> n;
+    vll a(n);
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
-        sum+=a[i];
-        pref[i]=pref[i-1];
-        pref[i]+=a[i];
     }
 
-    for(int i=0;i<q;i++){
-        ll l,r,k;
-        cin>>l>>r>>k;
-        ll ans = pref[n]-(pref[r]-pref[l-1])+k*(r-l+1);
-        if(ans%2==1){
-            yes;
-        }else{
-            no;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 1)
+        {
+            a[i]++;
         }
     }
-    
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] % a[i - 1] == 0)
+        {
+            a[i]++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << nl;
 }
 
 int main()
